ad51f552ecadbf0ab6a5c50f84b4201bd1131bde
myeongjunPark <mjooon98@kakao.com>
2022-09-07 23:08:41 +0900

    add button, copyright

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
 import "../css/marque.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Copyright = styled.div`
+  font-size: 11px;
+  margin: 10px;
+`;
 
 function Home() {
   return (
@@ -23,6 +29,17 @@ function Home() {
         <Link to="/random" className="btn btn-success btn-lg">
           뭐 먹지?
         </Link>
+        <hr />
+        <a className="btn btn-default" href="https://github.com/MyeongjunPark" target="_blank">
+          깃허브
+        </a>
+        <a className="btn btn-default" href="https://mjooon.tistory.com/" target="_blank">
+          블로그
+        </a>
+        <a className="btn btn-default" href="https://bit.ly/3Q2Miuo" target="_blank">
+          포트폴리오
+        </a>
+        <Copyright>이 페이지에는 카페24(주)이 제공한 "카페24 아네모네"가 적용되어 있습니다.</Copyright>
       </div>
     </div>
   );
